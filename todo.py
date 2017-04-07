2ffcb42cd7f58f57845cae98b38def51c8c8c839
Sepler Gábor <sepler.gabor@gmail.com>
2017-04-07 14:57:03 +0200

    feat(user authentication) and colored texts

--- todo.py
+++ todo.py
@@ -1,5 +1,6 @@
 import sys
 import os
+from termcolor import colored, cprint
 
 class Controller():
 
@@ -22,44 +23,62 @@ class Controller():
                 display.usage_print()
             else:
                 model.db_opener()
-                if self.list_argv[0] == '-l':
-                    display.list_printer()
-                elif self.list_argv[0] == '-c':
-                    model.db_checker(self.list_argv[1])
-                    model.db_updater()
-                    display.list_printer()
-                elif self.list_argv[0] == '-a':
-                    if len(self.list_argv) == 1:
-                        display.error_add_remove()
-                    else:
-                        model.db_adder(' '.join(self.list_argv[1:]))
+                if model.auth == True:
+                    if self.list_argv[0] == '-l':
                         display.list_printer()
-                elif self.list_argv[0] == '-e':
-                    model.db_eraser()
-                    display.list_eraser()
-                elif self.list_argv[0] == '-r':
-                    if len(self.list_argv) == 1:
-                        display.error_add_remove()
+                    elif self.list_argv[0] == '-c':
+                        if len(self.list_argv) == 1:
+                            display.error_missing_arg()
+                        else:
+                            model.db_checker(self.list_argv[1])
+                            model.db_updater()
+                            display.list_printer()
+                    elif self.list_argv[0] == '-a':
+                        if len(self.list_argv) == 1:
+                            display.error_missing_arg()
+                        else:
+                            model.db_adder(' '.join(self.list_argv[1:]))
+                            display.list_printer()
+                    elif self.list_argv[0] == '-e':
+                        model.db_eraser()
+                        display.list_eraser()
+                    elif self.list_argv[0] == '-r':
+                        if len(self.list_argv) == 1:
+                            display.error_missing_arg()
+                        else:
+                            model.db_remover(self.list_argv[1])
+                            model.db_updater()
+                            display.list_printer()
                     else:
-                        model.db_remover(self.list_argv[1])
-                        model.db_updater()
-                        display.list_printer()
-
-                else:
-                    display.error_argument()
-                    display.usage_print()
-
+                        display.error_argument()
+                        display.usage_print()
 
 class Model():
 
     def __init__(self):
+        self.task_list_raw = []
         self.task_list = []
+        self.auth = False
 
     def db_opener(self):
-        self.file = open('db.txt', 'r+')
-        self.task_list_raw = self.file.readlines()
-        for task in self.task_list_raw:
-            self.task_list.append(task.split('|||'))
+        self.user_name = input('Please enter your user name: ')
+        self.password = input('Password: ')
+        try:
+            self.file = open(str(self.user_name)+'.txt', 'r+')
+        except FileNotFoundError:
+            self.file = open(str(self.user_name)+'.txt', 'w')
+            self.file.write(str(self.password) + '\n')
+            display.print_db_inited()
+        self.file.close()
+        self.file = open(str(self.user_name)+'.txt', 'r+')
+        if self.file.readline().rstrip() == self.password:
+            self.task_list.append(self.password)
+            self.task_list_raw = self.file.readlines()
+            for task in self.task_list_raw:
+                self.task_list.append(task.split('|||'))
+            self.auth = True
+        else:
+            display.error_user()
 
     def db_adder(self, task_to_add):
         self.task_list.append(['0', str(task_to_add) + '\n'])
@@ -70,12 +89,13 @@ class Model():
         self.eraser = input('Are you sure? (Y/N) ')
         if self.eraser == 'Y':
             self.file.close()
-            self.file = open('db.txt', 'w')
+            self.file = open(str(self.user_name)+'.txt', 'w')
+            self.file.write(self.password + '\n')
             self.file.close()
 
     def db_checker(self, num):
         try:
-            self.task_list[int(num)-1][0] = '1'
+            self.task_list[int(num)][0] = '1'
         except IndexError:
             display.error_index()
         except ValueError:
@@ -83,15 +103,16 @@ class Model():
 
     def db_updater(self):
         self.file.close()
-        self.file = open('db.txt', 'w')
-        for i in range(len(self.task_list)):
+        self.file = open(str(self.user_name)+'.txt', 'w')
+        self.file.write(self.password + '\n')
+        for i in range(1, len(self.task_list)):
             self.file.write(self.task_list[i][0] + '|||' + self.task_list[i][1])
         self.file.close()
         return self.task_list
 
     def db_remover(self, num):
         try:
-            self.task_list.remove(self.task_list[int(num)-1])
+            self.task_list.remove(self.task_list[int(num)])
         except IndexError:
             display.error_index()
         except ValueError:
@@ -104,24 +125,26 @@ class Display():
         self.checked = '[X] '
 
     def usage_print(self):
-        print(' Python Todo application\n ======================= \n \n Command line arguments: \n')
-        print(' -h   Help on command line arguments.')
-        print(' -l   Lists all the tasks')
-        print(' -a   Adds a new task')
-        print(' -r   Removes a task. Enter task number.')
-        print(' -c   Completes a task. Enter task number.')
-        print(' -e   Empty task list')
+        cprint(' Python Todo application\n ======================= \n \n Command line arguments: \n', 'yellow')
+        cprint(' -h   Help on command line arguments.', 'yellow')
+        cprint(' -l   Lists all the tasks', 'yellow')
+        cprint(' -a   Adds a new task', 'yellow')
+        cprint(' -r   Removes a task. Enter task number.', 'yellow')
+        cprint(' -c   Completes a task. Enter task number.', 'yellow')
+        cprint(' -e   Empty task list', 'yellow')
 
     def list_printer(self):
-        if model.task_list == []:
-            print('No todos for today!')
+        if len(model.task_list) == 1:
+            cprint('No todos for ' + str(model.user_name) + '!', 'white', 'on_blue')
         else:
-            print('\nThings to do:')
+            cprint('\nThings to do for ' + str(model.user_name) + ':', 'white', 'on_blue')
             for i in range(len(model.task_list)):
                 if model.task_list[i][0] == '0':
-                    print(str(i+1) + ' - ' + self.not_checked + model.task_list[i][1][:-1])
+                    print(str(i) + ' - ' + self.not_checked + model.task_list[i][1][:-1])
+                elif model.task_list[i][0] == '1':
+                    print(str(i) + ' - ' + self.checked + model.task_list[i][1][:-1])
                 else:
-                    print(str(i+1) + ' - ' + self.checked + model.task_list[i][1][:-1])
+                    pass
 
     def list_eraser(self):
         if model.eraser == 'Y':
@@ -130,16 +153,22 @@ class Display():
             print('Exit without erasing.')
 
     def error_argument(self):
-        print('\nUnsupported argument. \n')
+        cprint('\nUnsupported argument. \n', 'red')
 
-    def error_add_remove(self):
-        print('\nUnable to perform: no task provided. \n')
+    def error_missing_arg(self):
+        cprint('\nUnable to perform: no task provided. \n', 'red')
 
     def error_index(self):
-        print('\nUnable to perform: index is out of bound. \n')
+        cprint('\nUnable to perform: index is out of bound. \n', 'red')
 
     def error_value(self):
-        print('\nUnable to perform: index is not a number. \n')
+        cprint('\nUnable to perform: index is not a number. \n', 'red')
+
+    def error_user(self):
+        cprint('\nWrong password.\n', 'red')
+
+    def print_db_inited(self):
+        cprint('\nTask list successfully created.\n', 'green')
 
 display = Display()
 model = Model()
